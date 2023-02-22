5c89c5f6382baef98a3f00dc2dd25cfba3a79179
Ruonavaara Markku <markku.ruonavaara@haaga-helia.fi>
2023-02-22 15:08:03 +0200

    Fixed router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { StrictMode } from 'react';
-import { createHashRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 
 // language specific data
 import data_fi from "./resources/data_fi.json";
@@ -8,7 +8,7 @@ import data_en from "./resources/data_en.json";
 
 import CriteriaTable from './components/CriteriaTable';
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/fi",
     element: <CriteriaTable data={data_fi} />
